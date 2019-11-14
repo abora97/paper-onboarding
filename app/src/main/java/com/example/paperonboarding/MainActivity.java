f6694d60c605ee01af515e733d41fde0eb9ebaee
Abora <https://github.com/abora97>
2019-11-14 23:16:23 +0200

    finish youTube tutorial

--- app/src/main/java/com/example/paperonboarding/MainActivity.java
+++ app/src/main/java/com/example/paperonboarding/MainActivity.java
@@ -1,15 +1,118 @@
-
 package com.example.paperonboarding;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.text.Html;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @BindView(R.id.view_pager)
+    ViewPager viewPager;
+    @BindView(R.id.linear)
+    LinearLayout linear;
+    @BindView(R.id.btn_back)
+    Button btnBack;
+    @BindView(R.id.btn_next)
+    Button btnNext;
+    private int mCurrentPage;
+    private TextView[] mDots;
+
+    private SliderAdapter sliderAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        initSlider();
+        addDotsIndicator(0);
+        viewPager.addOnPageChangeListener(viewListener);
+        btnBack.setOnClickListener(this);
+        btnNext.setOnClickListener(this);
+    }
+
+    private void initSlider() {
+        sliderAdapter = new SliderAdapter(this);
+        viewPager.setAdapter(sliderAdapter);
+    }
+
+
+    private void addDotsIndicator(int position) {
+        mDots = new TextView[3];
+        linear.removeAllViews();
+        for (int i = 0; i < mDots.length; i++) {
+            mDots[i] = new TextView(this);
+            mDots[i].setText(Html.fromHtml("&#8226;"));
+            mDots[i].setTextSize(35);
+            mDots[i].setTextColor(getResources().getColor(R.color.transparentWhite));
+
+            linear.addView(mDots[i]);
+        }
+
+        if (mDots.length > 0) {
+            mDots[position].setTextColor(getResources().getColor(R.color.white));
+        }
+
+    }
+
+    ViewPager.OnPageChangeListener viewListener = new ViewPager.OnPageChangeListener() {
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+        }
+
+        @Override
+        public void onPageSelected(int position) {
+            addDotsIndicator(position);
+            mCurrentPage = position;
+            if (position == 0) {
+                btnNext.setEnabled(true);
+                btnBack.setEnabled(false);
+                btnBack.setVisibility(View.INVISIBLE);
+
+                btnNext.setText("Next");
+                btnBack.setText("");
+            } else if (position == mDots.length - 1) {
+                btnNext.setEnabled(true);
+                btnBack.setEnabled(true);
+                btnBack.setVisibility(View.VISIBLE);
+
+                btnNext.setText("Finish");
+                btnBack.setText("Back");
+            } else {
+                btnNext.setEnabled(true);
+                btnBack.setEnabled(true);
+                btnBack.setVisibility(View.VISIBLE);
+
+                btnNext.setText("Next");
+                btnBack.setText("Back");
+            }
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int state) {
+
+        }
+    };
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btn_next:
+                viewPager.setCurrentItem(mCurrentPage+1);
+                break;
+            case R.id.btn_back:
+                viewPager.setCurrentItem(mCurrentPage-1);
+                break;
+        }
     }
 }
---
+++ app/src/main/java/com/example/paperonboarding/SliderAdapter.java
@@ -0,0 +1,58 @@
+package com.example.paperonboarding;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.viewpager.widget.PagerAdapter;
+
+public class SliderAdapter extends PagerAdapter {
+
+    Context context;
+    LayoutInflater layoutInflater;
+
+    public SliderAdapter(Context context) {
+        this.context = context;
+    }
+
+    //Arrays
+    public int[] slider_image = {R.drawable.eat_icon, R.drawable.sleep_icon, R.drawable.work_icon};
+    public String[] slider_header = {"Eat", "Sleep", "Work"};
+
+    @Override
+    public int getCount() {
+        return slider_header.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return view == (LinearLayout) object;
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+        layoutInflater = (LayoutInflater) context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
+        View view = layoutInflater.inflate(R.layout.slide_layout, container, false);
+
+        ImageView slideImage = view.findViewById(R.id.iv_slide);
+        TextView slideText = view.findViewById(R.id.tv_slide);
+
+        slideImage.setImageResource(slider_image[position]);
+        slideText.setText(slider_header[position]);
+
+        container.addView(view);
+        return view;
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        container.removeView((LinearLayout) object);
+    }
+}
